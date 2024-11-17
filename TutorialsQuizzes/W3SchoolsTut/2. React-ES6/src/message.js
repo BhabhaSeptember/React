6babd9a29fb819dbfd04a0292e7f57c71ed13e32
BhabhaSeptember <septemberbhabha@gmail.com>
2024-11-17 17:05:47 +0200

    commit

---
+++ TutorialsQuizzes/W3SchoolsTut/2. React-ES6/src/message.js
@@ -0,0 +1,8 @@
+//DEFAULT EXPORTS
+const message = () => {
+    const name = "Akhona";
+    const age = 40;
+    return name + ' is ' + age + ' years old.';
+};
+
+export default message;
---
+++ TutorialsQuizzes/W3SchoolsTut/2. React-ES6/src/person.js
@@ -0,0 +1,10 @@
+//NAMED EXPORTS
+//EXAMPLE1: INLINE-INDIVIDUALLY
+export const name = "Bhabha";
+export const age = 28;
+
+//EXAMPLE2: ALL AT ONCE
+// const name2 = "Tracy";
+// const age2 = 19;
+
+// export { name2, age2};
