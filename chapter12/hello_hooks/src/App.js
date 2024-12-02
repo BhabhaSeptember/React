cef525bb891e4446b15fac60e4c68ba0e862d024
BhabhaSeptember <septemberbhabha@gmail.com>
2024-12-02 09:24:42 +0200

    commit

--- chapter12/hello_hooks/src/App.js
+++ chapter12/hello_hooks/src/App.js
@@ -29,28 +29,38 @@
 
 
 //FUNCTION-BASED COMPONENT
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import useFetch from './useFetch';
+import Users from './Users';
 
 const App = () => {
   const postsUrl = "https://jsonplaceholder.typicode.com/posts";
   const todosUrl = "https://jsonplaceholder.typicode.com/todos";
   const [requested, setRequested] = useState(postsUrl);
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const data = useFetch(requested);
 
-  useEffect( ()=> {
-    fetch(requested)
-    .then(response => response.json())
-    .then(data => setData(data))
-  }, [requested]);
+  // useEffect( ()=> {
+  //   fetch(requested)
+  //   .then(response => response.json())
+  //   .then(data => setData(data))
+  // }, [requested]);
 
   return (
     <div>
+      <h1><u>Users</u></h1>
+      <Users/>
+      <hr/>
       <Button variant="link" onClick={()=>setRequested(postsUrl)}>Posts</Button>
       <Button variant="link" onClick={()=>setRequested(todosUrl)}>Todos</Button>
+      
       <br/>
-      Requested: { requested }
+      <br/>
+      <b>Requested:</b> <u>{ requested }</u>
+      <br/>
+      <br/>
       <ul>
         {data.map(el => (
           <li key={el.id}>{el.title}</li>
